43d9ae6d600549c05bc2a9043fd7fbc6650b8036
arplx <93895162+arplx@users.noreply.github.com>
2023-12-26 19:39:47 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // objects
 let movies = [
     {
-        name: 'WI  vs IND 317/8 (50)',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: '',
+        des: '',
         image: 'images/slider 2.PNG'
     },
     {
-        name: 'Rudra: The Edge of Darkness',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: '',
+        des: '',
         image: 'images/slider 1.PNG'
     },
     {
-        name: 'The Book of Boba Fett',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: '',
+        des: '',
         image: 'images/slider 3.PNG'
     },
     {
-        name: 'Raya and the Last Dragon',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: '',
+        des: '',
         image: 'images/slider 4.PNG'
     },
     {
-        name: 'Luca',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: '',
+        des: '',
         image: 'images/slider 5.PNG'
     }
 ];
